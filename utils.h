8b3ad02f6d1785a521469129cd83386b498fc241
Daljit <daljit7991@gmail.com>
2024-10-28 15:18:09 +0000

    Add function to convert from degrees to radians

--- utils.h
+++ utils.h
@@ -17,4 +17,10 @@ std::string readFile(const std::filesystem::path &filePath, ReadFileMode mode = 
 
 // Replaces all placeholders of the form {{value}} with the given value
 std::string replacePlaceholder(std::string_view str, std::string_view placeholder, std::string_view value);
+
+template<typename T>
+T degreesToRadians(T degrees) {
+    return degrees * 3.14159265358979323846 / 180.0;
+}
+
 }
