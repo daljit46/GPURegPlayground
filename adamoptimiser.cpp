3404f4a0752cf7d13d6dbff6f12480601c5bb4c4
Daljit <daljit7991@gmail.com>
2024-11-25 11:23:31 +0000

    Fix narrowing conversion

--- adamoptimiser.cpp
+++ adamoptimiser.cpp
@@ -15,13 +15,12 @@ AdamOptimizer::AdamOptimizer(const std::vector<Parameter>& parameters, float bet
 }
 
 const std::vector<AdamOptimizer::Parameter>& AdamOptimizer::step(const std::vector<float> &gradients)
-{
     ++m_timeStep;
     for(size_t i = 0; i < m_parameters.size(); ++i) {
         m_firstMoments[i] = m_beta1 * m_firstMoments[i] + (1.0F - m_beta1) * gradients[i];
         m_secondMoments[i] = m_beta2 * m_secondMoments[i] + (1.0F - m_beta2) * gradients[i] * gradients[i];
-        const auto firstMomentCorrected = m_firstMoments[i] / (1.0F - std::pow(m_beta1, m_timeStep));
-        const auto secondMomentCorrected = m_secondMoments[i] / (1.0F - std::pow(m_beta2, m_timeStep));
+        const float firstMomentCorrected = m_firstMoments[i] / (1.0F - std::pow(m_beta1, m_timeStep));
+        const float secondMomentCorrected = m_secondMoments[i] / (1.0F - std::pow(m_beta2, m_timeStep));
         m_parameters[i].value -= m_parameters[i].learning_rate * firstMomentCorrected / (std::sqrt(secondMomentCorrected) + m_epsilon);
     }
 
