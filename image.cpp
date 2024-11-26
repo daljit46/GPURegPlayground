6d629e2666d609da182595ef5c883f6ffe1f4c99
Daljit <daljit7991@gmail.com>
2024-11-26 09:05:23 +0000

    Add image.cpp

---
+++ image.cpp
@@ -0,0 +1,34 @@
+#include "image.h"
+#include "nifti1_io.h"
+#include <stdexcept>
+
+
+NiftiImage::NiftiImage(nifti_image *handle) : m_handle(handle)
+{
+    switch (handle->datatype) {
+    case DT_UINT8:
+        break;
+    default:
+        throw std::runtime_error("Image data type not supported");
+    }
+
+    width = handle->nx;
+    height = handle->ny;
+    depth = handle->nz;
+}
+
+NiftiImage::~NiftiImage()
+{
+    nifti_image_free(m_handle);
+}
+
+uint8_t *NiftiImage::data() const
+{
+    return static_cast<uint8_t*>(m_handle->data);
+}
+
+uint8_t NiftiImage::at(size_t index) const
+{
+    auto *data = static_cast<uint8_t*>(m_handle->data);
+    return data[index];
+}
