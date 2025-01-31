7ec52d7556aad96ea40c8c3fc37da66812641249
Daljit Singh <daljit7991@gmail.com>
2025-01-31 19:39:10 +0000

    Lower convergence threshold for NCC

--- main_3d.cpp
+++ main_3d.cpp
@@ -620,7 +620,7 @@ SingleLevelResult registerAtSingleResolutionNCC(
 
         if(i > 10) {
             auto mean = std::accumulate(nccHistory.end()-10, nccHistory.end(), 0.0F) / 10;
-            if (std::abs(ncc - mean) < 0.001) {
+            if (std::abs(ncc - mean) < 1e-4) {
                 break;
             }
         }
